918cb8bf9dabe90511835371611dd2c2514664d9
ailojay <pauledukpe@yahoo.com>
2025-09-19 10:47:15 +0100

    created state bucket and locked with dynamodb

---
+++ environments/secops/main.tf
@@ -0,0 +1,9 @@
+module "iam" {
+  source = "./iam"  # This references your local iam/ subdirectory
+  github_org_name  = var.github_org_name
+  github_repo_name = var.github_repo_name
+}
+
+module "cloudtrail" {
+  source = "./cloudtrail"  
+}
---
+++ environments/secops/variables.tf
@@ -0,0 +1,12 @@
+variable "github_org_name" {
+  description = "The name of the GitHub organization/user that owns the repository"
+  type        = string
+  default     = "ailojay" 
+  
+}
+
+variable "github_repo_name" {
+  description = "The name of the GitHub repository"
+  type        = string
+  default     = "Perp"
+}

1a6cc910766fbf0865a360dee601088828595af1
ailojay <pauledukpe@yahoo.com>
2025-09-19 01:39:34 +0100

    fixed oidc thumbprint

--- environments/secops/iam/github-oidc-role.tf
+++ environments/secops/iam/github-oidc-role.tf
@@ -26,6 +26,6 @@ resource "aws_iam_openid_connect_provider" "github" {
   ]
 
   thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1" # GitHub's OIDC thumbprint
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd" # GitHub's OIDC thumbprint
   ]
 }

89f5c3eeb29b4f51c68907fa7f5d3dc8190b4291
ailojay <pauledukpe@yahoo.com>
2025-09-19 01:32:11 +0100

    fixed oidc for secops

--- environments/secops/iam/github-oidc-role.tf
+++ environments/secops/iam/github-oidc-role.tf
@@ -15,3 +15,17 @@ resource "aws_iam_role_policy_attachment" "github_actions_secops_admin" {
 }
 
 # Output the role ARN so we can easily add it to our GitHub workflow
+
+
+# Create the OIDC Identity Provider for GitHub
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com" # This is the intended audience of the GitHub OIDC tokens
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1" # GitHub's OIDC thumbprint
+  ]
+}

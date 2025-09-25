c67b15cfbe965e01e39cce7a22c353a624a57170
ailojay <pauledukpe@yahoo.com>
2025-09-25 15:13:41 +0100

    fixed config

--- global/delegation.tf
+++ global/delegation.tf
@@ -1,34 +1,22 @@
-# Delegate Access Analyzer Administration
-resource "aws_organizations_organization" "org" {
-  feature_set = "ALL"
-
-  aws_service_access_principals = [
-    "config.amazonaws.com",
-    "cloudtrail.amazonaws.com",
-    "securityhub.amazonaws.com",
-    "access-analyzer.amazonaws.com",
-    "guardduty.amazonaws.com"
-  ]
-}
-
-
-
+# Delegated Administrators for AWS Services
+# Access Analyzer
 resource "aws_organizations_delegated_administrator" "access_analyzer" {
-  account_id        = var.secops_account_id # your delegated account
+  account_id        = var.secops_account_id
   service_principal = "access-analyzer.amazonaws.com"
 }
 
-# Delegate GuardDuty Administration
+# Config
+resource "aws_organizations_delegated_administrator" "config" {
+  account_id        = var.secops_account_id
+  service_principal = "config.amazonaws.com"
+}
+
+# GuardDuty
 resource "aws_guardduty_organization_admin_account" "secops" {
   admin_account_id = var.secops_account_id
 }
 
-# Delegate Security Hub Administration
+# Security Hub
 resource "aws_securityhub_organization_admin_account" "secops" {
   admin_account_id = var.secops_account_id
-}
-
-resource "aws_organizations_delegated_administrator" "config" {
-  account_id        = var.secops_account_id
-  service_principal = "config.amazonaws.com"
 }

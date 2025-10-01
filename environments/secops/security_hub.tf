c05eb9f71b61652031a452ad1310a002862d4112
ailojay <pauledukpe@yahoo.com>
2025-10-01 12:28:20 +0100

    added life cycle rule to securityhub.tf

--- environments/secops/security_hub.tf
+++ environments/secops/security_hub.tf
@@ -22,4 +22,7 @@ resource "aws_securityhub_standards_subscription" "cis" {
 
 resource "aws_securityhub_standards_subscription" "foundational" {
   standards_arn = "arn:aws:securityhub:${var.region}::standards/aws-foundational-security-best-practices/v/1.0.0"
+    lifecycle {
+    prevent_destroy = true
+  }
 }

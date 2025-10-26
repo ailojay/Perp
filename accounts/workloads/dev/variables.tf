9d49d2240389e1333c7f0b85a7199608f1a1985a
ailojay <pauledukpe@yahoo.com>
2025-10-26 20:40:55 +0100

    fix: add default values to dev account variables

--- accounts/workloads/dev/variables.tf
+++ accounts/workloads/dev/variables.tf
@@ -1,31 +1,37 @@
 variable "project_name" {
   description = "Project name"
   type        = string
+  default     = "perp"
 }
 
 variable "environment" {
   description = "Environment name (dev, staging, prod)"
   type        = string
+  default     = "dev"
 }
 
 variable "region" {
   description = "AWS region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "state_bucket" {
   description = "S3 bucket for remote state"
   type        = string
+  default     = "perp-dev-state"
 }
 
 variable "vpcproject_name" {
   description = "VPC project name"
   type        = string
+  default     = "perp-dev-vpc"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
   type        = string
+  default     = "perp-dev-instance"
 }
 
 variable "cidr_block" {

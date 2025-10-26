227b65053a115753c8d1b7848c3fda03ec516bb9
ailojay <pauledukpe@yahoo.com>
2025-10-26 20:34:29 +0100

    fixed oidc issuea and commented out network module in dev account

--- accounts/workloads/dev/budgets.tf
+++
@@ -1,7 +0,0 @@
-module "dev_budget" {
-  source            = "../../modules/shared/budgets"
-  name              = "dev"
-  amount            = "5"
-  linked_account_id = var.dev_account_id
-  alert_email       = var.alert_email
-}
--- accounts/workloads/dev/main.tf
+++ accounts/workloads/dev/main.tf
@@ -1,6 +1,6 @@
-module "network" {
-  source       = "../../modules/network"
-  project_name = var.project_name
-  environment  = var.environment
-  cidr_block   = var.cidr_block
-}
+# module "network" {
+#   source       = "../../modules/network"
+#   project_name = var.project_name
+#   environment  = var.environment
+#   cidr_block   = var.cidr_block
+# }
